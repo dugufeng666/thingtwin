e788755388972dbab713f207f898f74877e1f1cd
独孤风 <133836769+dugufeng666@users.noreply.github.com>
2025-05-20 12:44:48 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig(({ command }) => ({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
   },
   build: {
     outDir: 'dist',

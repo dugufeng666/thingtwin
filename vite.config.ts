6cf9aa5a73034af4a5f12167625837cc3f57ba20
独孤风 <133836769+dugufeng666@users.noreply.github.com>
2025-05-20 11:13:36 +0800

    Update GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
